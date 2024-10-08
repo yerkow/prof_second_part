8102a84ffde6125942952592463eabbfe9d5ac6f
Tsk-Andrey078 <tskandrey2002.@gmail.com>
2024-10-08 12:19:28 +0500

    Fix

--- prof_second/prof_app/models.py
+++ prof_second/prof_app/models.py
@@ -56,8 +56,8 @@ class ProfMember(models.Model):
     total_work_experience = models.DateField()
     org_work_experience = models.DateField()
     union_membership_date = models.DateField()
-    awards = models.TextField()
-    vacation = models.TextField()
+    awards_list = models.TextField()
+    vacation_list = models.TextField()
     phone = models.CharField(max_length = 20)
     email = models.CharField(max_length = 255)
 
